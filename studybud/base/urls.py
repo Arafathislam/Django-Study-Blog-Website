727fe735c4fe936e49444e9d18d35f220dcfbfc2
Arafathislam <islamarafath315@gmail.com>
2022-04-19 03:11:48 +0600

    working

--- studybud/base/urls.py
+++ studybud/base/urls.py
@@ -6,6 +6,7 @@ urlpatterns=[
     path('logout/',views.logoutUser,name="logout"),
     path('login/',views.loginPage,name="login"),
     path('register/',views.registerPage,name="register"),
+    path('profile/<str:pk>/', views.userProfile, name="user-profile"),
 
 
     path('',views.home,name="home"),
--- studybud/base/views.py
+++ studybud/base/views.py
@@ -76,7 +76,7 @@ def home(request):
         ) 
     topics=Topic.objects.all()
     room_count=rooms.count()
-    room_messages=Message.objects.all()
+    room_messages=Message.objects.filter(room__topic__name__icontains=q)
     context={'rooms':rooms,'topics':topics,'room_count':room_count,'room_messages':room_messages}
  
     return render(request,'base/home.html',context)
@@ -97,6 +97,21 @@ def room(request,pk):
     context= {'room':room , 'room_messages':room_messages ,'participants':participants}
 
     return render(request,'base/room.html',context)
+
+
+def userProfile(request,pk):
+    user=User.objects.get(id=pk)
+    rooms=user.room_set.all()
+    room_message=user.message_set.all()
+    topics=Topic.objects.all()
+    context={'user':user,'rooms':rooms,'room_message':room_message,'topics':topics}
+
+    return render(request,'base/profile.html', context)
+
+
+
+
+
 
 @login_required(login_url='/login')
 def createroom(request):
